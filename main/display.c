0f87b9cefc89a254b8478d13b6c35d447955a270
calebe94 <calebe.sep@hotmail.com>
2019-08-21 18:45:51 -0300

    added task to control screen output

--- main/display.c
+++ main/display.c
@@ -4,12 +4,49 @@
 #include <stdbool.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/queue.h"
-#include <time.h>
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+
+#include "ssd1306.h"
+#include "lcd/oled_sh1106.h"
 
 #include "display.h"
-#include "freertos/semphr.h"
+#include "encoder.h"
+
 
 static char tag[] = "display";
 
-void display_setup(void){
+static int pulses = 0;
+
+char pulses_string[20]; 
+char turns_string[20];
+
+static void task_display_print_info(void *arg)
+{
+    uint32_t io_num;
+    
+    for(;;) 
+    {
+        if(xQueueReceive(encoders_queue, &io_num, portMAX_DELAY)) 
+        {
+            if(io_num == ENCODER_1)
+            {
+                pulses++;
+                printf("pulses %d - turns %d \n", pulses, (int)pulses/16);
+                ssd1306_printFixed(0,  8, "Encoder 1", STYLE_BOLD);
+                sprintf(pulses_string, "pulses: %d", pulses);
+                sprintf(turns_string, "turns: %d", (int)pulses/16);
+                ssd1306_printFixed(0, 24, pulses_string, STYLE_NORMAL);
+                ssd1306_printFixed(0, 38, turns_string, STYLE_NORMAL);
+            }
+        }
+    }    
+}
+
+void display_setup(void)
+{
+    sh1106_128x64_i2c_init();
+    ssd1306_setFixedFont(ssd1306xled_font6x8);
+    ssd1306_clearScreen();
+    xTaskCreate(task_display_print_info, "task_display_print_info", 2048, NULL, 10, NULL);
 }
--- main/rally-robot.c
+++ main/rally-robot.c
@@ -7,40 +7,17 @@
 
 #include "encoder.h"
 #include "motor.h"
-#include "ssd1306.h"
-#include "lcd/oled_sh1106.h"
-
-static int pulses = 0;
-
-static void encoders_handle(void* arg)
-{
-    uint32_t io_num;
-    for(;;) 
-    {
-        if(xQueueReceive(encoders_queue, &io_num, portMAX_DELAY)) 
-        {
-            if(io_num == ENCODER_1)
-            {
-                pulses++;
-                printf("pulses %d - turns %d \n", pulses, (int)pulses/16);
-            }
-        }
-    }
-}
+#include "display.h"
 
 void setup()
 {
     encoder_init();
-    xTaskCreate(encoders_handle, "encoders_handle", 2048, NULL, 10, NULL);
+    display_setup();
 }
 
 int app_main(void)
 {
     setup();
-    sh1106_128x64_i2c_init();
-    ssd1306_setFixedFont(ssd1306xled_font6x8);
-    ssd1306_clearScreen();
-
 
     while(1)
     {
